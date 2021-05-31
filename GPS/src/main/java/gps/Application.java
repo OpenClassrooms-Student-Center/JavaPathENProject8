1763a16abde5bf3da08d9958567bf009d8c67f67
Antoine-CJ-Thomas <antoine.cj.thomas@gmail.com>
2021-05-31 17:08:30 +0200

    All modules containers setup

--- GPS/src/main/java/gps/Application.java
+++ GPS/src/main/java/gps/Application.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.util.Locale;
+
 @SpringBootApplication
 @EnableFeignClients("TourGuide-Gps")
 public class Application {
+
+    public Application() {
+
+        Locale.setDefault(Locale.US);
+    }
 
     public static void main(String[] args) {
 
